4688ed452f1447a203862270e61b896ea6488eb4
bassy3260@naver.com <bassy3260@naver.com>
2022-04-12 00:03:11 +0900

    글쓰기 기능 화면 구현

---
+++ app/src/main/java/com/aaa/aaa/writeActivity.java
@@ -0,0 +1,84 @@
+/**
+ * 마이페이지 액티비티
+ **/
+package com.aaa.aaa;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+
+public class writeActivity extends AppCompatActivity {
+    Toolbar writeToolbar;
+
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_write);
+
+        ActionBar actionBar =getSupportActionBar();
+
+        Toolbar tb = (Toolbar) findViewById(R.id.write_toolbar);
+        setSupportActionBar(tb);//액션바를 툴바로 바꿔줌
+        getSupportActionBar().setTitle("글 쓰기");
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        // Spinner
+        Spinner categorySpinner = (Spinner) findViewById(R.id.category_spinner);
+        ArrayAdapter categoryAdapter = ArrayAdapter.createFromResource(this,
+                R.array.category_array, android.R.layout.simple_spinner_item);
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(categoryAdapter);
+
+
+
+    }
+
+    //추가된 소스, ToolBar에 menu.xml을 인플레이트함
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //return super.onCreateOptionsMenu(menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.community_write, menu);
+        return true;
+    }
+
+    //추가된 소스, ToolBar에 추가된 항목의 select 이벤트를 처리하는 함수
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                // User chose the "Settings" item, show the app settings UI...
+                finish();
+                break;
+            case R.id.menu_write:
+                // User chose the "Settings" item, show the app settings UI...
+                finish();
+                break;
+        }
+        return true;
+    }
+
+
+
+    public void toast(String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+
+}
