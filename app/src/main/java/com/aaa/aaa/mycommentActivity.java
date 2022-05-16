c3e97f9bea7a005fbc392b6560d1b2f6461de973
bassy3260 <bassy3260@gmail.com>
2022-05-17 02:09:46 +0900

    글쓰기, 글 페이지 수정 완료 카테고리별 게시글 가져오기 완료 마이페이지 백엔드 구현 중

---
+++ app/src/main/java/com/aaa/aaa/mycommentActivity.java
@@ -0,0 +1,69 @@
+package com.aaa.aaa;
+
+/** 게시글 리스트 (질문 답변) **/
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aaa.aaa.adpater.communityListViewAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class mycommentActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private communityListViewAdapter adapter;
+    private FirebaseFirestore database;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypost);
+        firebaseAuth=FirebaseAuth.getInstance();
+        user=firebaseAuth.getCurrentUser();
+
+        final ArrayList<writeInfo> postList = new ArrayList<>();
+        final ArrayList<String> postIdList = new ArrayList<>();
+        database = FirebaseFirestore.getInstance();
+
+        //FireStore에서 게시글 정보 받아오기
+        database.collection("comment")
+                // 카테고리에 따라 게시글 받아오기
+                .whereEqualTo("comment_uid", user.getUid())
+                //시간순 정렬
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                postIdList.add(document.getData().get("post_id").toString());
+                            }
+                        } else {
+                        }
+                    }
+                });
+
+
+
+    }
+}
---
+++ app/src/main/java/com/aaa/aaa/mypostActivity.java
@@ -0,0 +1,78 @@
+package com.aaa.aaa;
+
+/** 게시글 리스트 (질문 답변) **/
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aaa.aaa.adpater.communityListViewAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class mypostActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private communityListViewAdapter adapter;
+    private FirebaseFirestore database;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseUser user;
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        setContentView(R.layout.activity_mypost);
+        firebaseAuth=FirebaseAuth.getInstance();
+        user=firebaseAuth.getCurrentUser();
+        /** 리사이클러 뷰(게시글 리스트) 생성 **/
+        final ArrayList<writeInfo> postList = new ArrayList<>();
+        database = FirebaseFirestore.getInstance();
+
+        //FireStore에서 게시글 정보 받아오기
+        database.collection("post")
+                // 카테고리에 따라 게시글 받아오기
+                .whereEqualTo("uid", user.getUid())
+                //시간순 정렬
+                .orderBy("created", Query.Direction.DESCENDING)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                postList.add(new writeInfo(
+                                        document.getData().get("category").toString(),
+                                        document.getData().get("title").toString(),
+                                        document.getData().get("uid").toString(),
+                                        new Date(document.getDate("created").getTime()),
+                                        (ArrayList<String>) document.getData().get("content"),
+                                        document.getData().get("postKey").toString()));
+                            }
+                            //리사이클러 뷰 생성
+                            recyclerView = findViewById(R.id.mypostRecyclerView);
+                            recyclerView.setHasFixedSize(true);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(mypostActivity.this));
+                            adapter = new communityListViewAdapter(mypostActivity.this, postList);
+                            recyclerView.setAdapter(adapter);
+                        } else {
+                        }
+                    }
+                });
+    }
+}
